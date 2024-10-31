0872387bcef2b5be422d6168be00ace5280f8d4d
Ganeshsalgar <ganeshsalgar2002@gmail.com>
2024-10-31 18:59:20 +0530

    user controller changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,24 +26,27 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     // Step 4: Upload images if provided, and check for avatar
-    const avatarLocalPath = req.files?.avatar?.[0]?.path;
-    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
     
+
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required");
+        throw new ApiError(400, "Avatar file is required")
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = coverImageLocalPath ? await uploadOnCloudinary(coverImageLocalPath) : null;
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
-        throw new ApiError(400, "Avatar upload failed");
+        throw new ApiError(400, "Avatar file is required")
     }
+   
+
 
     // Step 5: Create user object and add entry to the database
     const user = await User.create({
